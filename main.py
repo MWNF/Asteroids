de3712d06a77bba7931d7957402cfaaba2fc6463
MWNF <mwnf01@gmail.com>
2025-03-02 13:32:11 -0600

    2: Added circleshape class, player class, drew player to the screen

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     while True:
         # This checks to see if the user closed the window and exit the game if they do
         for event in pygame.event.get():
@@ -10,11 +19,12 @@ def main():
                 return
         # fill the screen black
         screen.fill((0, 0, 0), rect=None, special_flags=0)
+        #draws player
+        player.draw(screen)
         #refresh the screen
         pygame.display.flip()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        #limits the FPS to 60 and takes the delta time return and divides to convert from ms to s
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
