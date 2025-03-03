1112991de98354753d9415a91cfb06ce036a0e74
MWNF <mwnf01@gmail.com>
2025-03-02 21:10:31 -0600

    5: Added asteroid field

--- main.py
+++ main.py
@@ -2,19 +2,26 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
+    asteroids = pygame.sprite.Group()
+    asteroid_field = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    field_of_asteroids = AsteroidField()
     while True:
         # This checks to see if the user closed the window and exit the game if they do
         for event in pygame.event.get():
@@ -27,7 +34,7 @@ def main():
         screen.fill((0, 0, 0), rect=None, special_flags=0)
         for draw in drawable:
             draw.draw(screen)
-        updateable.update(dt)
+        updatable.update(dt)
         #refresh the screen
         pygame.display.flip()
 
